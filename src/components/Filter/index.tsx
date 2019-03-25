c4720a52dff5a3489e2b15dfed830bbcf2c36152
github username <elis.paul7@gmail.com>
2019-03-25 18:18:42 -0400

    dropdown menu and hide/show functions added

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -3,15 +3,41 @@ import './index.css';
 
 class Filter extends React.Component {
 
+  state = {
+    displayMenu: false,
+  };
+
+  showDropdownMenu(event: React.SyntheticEvent<HTMLDivElement>): void {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+      document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+
   render(): JSX.Element {
     return (
-      <div id='filter-container'>
-        <ul className="dd-list">
-            <li className="dd-list-item">Item1</li>
-            <li className="dd-list-item">Item2</li>
-            <li className="dd-list-item">Item3</li>
-        </ul>
-      </div>
+      <div  className="dropdown" style = {{background:"red",width:"200px"}} >
+        <div className="button" onClick={this.showDropdownMenu}> My Setting </div>
+          { this.state.displayMenu ? (
+            <ul>
+              <li><a className="active" href="#Create Page">Create Page</a></li>
+              <li><a href="#Manage Pages">Manage Pages</a></li>
+              <li><a href="#Create Ads">Create Ads</a></li>
+              <li><a href="#Manage Ads">Manage Ads</a></li>
+              <li><a href="#Activity Logs">Activity Logs</a></li>
+              <li><a href="#Setting">Setting</a></li>
+              <li><a href="#Log Out">Log Out</a></li>
+            </ul>
+            ): ( null )
+          }
+    </div>
     );
   }
 }
